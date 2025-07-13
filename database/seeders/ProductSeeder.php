17b506325c4a03e7d1b8eea4f219953d6a23f5c6
Cliente <gabrielbromo@gmail.com>
2025-07-13 18:52:26 -0300

    feat: update ProductSeeder to import products from external API

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,17 +2,38 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
+use App\Models\Product; 
 
 class ProductSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
-    {
-        \App\Models\Product::factory()->count(20)->create();
-    }
+    { 
 
+        $response = Http::withHeaders([
+            'Authorization' => 'wQ8ehU2x4gj93CH9lMTnelQO3GcFvLzyqn8Fj3WA0ffQy57I60',
+        ])->get('https://luvinco.proxy.beeceptor.com/products');
+
+        if ($response->successful()) {
+            foreach ($response->json() as $item) {
+                Product::updateOrCreate(
+                    ['product_id' => $item['product_id']],
+                    [
+                        'name' => $item['name'],
+                        'description' => $item['description'],
+                        'price' => $item['price'],
+                        'category' => $item['category'],
+                        'brand' => $item['brand'],
+                        'stock' => $item['stock'],
+                        'image_url' => 'https://placehold.co/600x600?text=' . urlencode($item['name']) . '&font=roboto',
+                    ]
+                );
+            }
+
+            $this->command->info('✅ Produtos importados com imagens fake.');
+        } else {
+            $this->command->error('❌ Falha ao importar produtos da API externa.');
+        }
+    }
 }
